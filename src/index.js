ebec3212845bfdeb928dd323dc5bddc4d695a2a8
Gio d'Amelio <giodamelio@gmail.com>
2015-01-09 21:19:39 -0800

    Basic cli inteface with commander

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-console.log(`1 + 1 = ${1 + 1}`);
+var commander = require("commander");
+
+var version = require("../package.json").version;
+
+commander
+    .version(version);
+
+commander
+    .command("server")
+    .description("Main game server")
+    .action(function(env) {
+        console.log(env);
+    });
+
+commander
+    .parse(process.argv);
 
