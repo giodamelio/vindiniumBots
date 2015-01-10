9c979fcb3875141b241b18f2d8cd334b434772da
Gio d'Amelio <giodamelio@gmail.com>
2015-01-10 00:20:36 -0800

    Load config and send to game

---
+++ src/game/index.js
@@ -0,0 +1,8 @@
+class Game {
+    constructor(key, turns=undefined, map=undefined) {
+        console.log(arguments);
+    }
+}
+
+module.exports = Game;
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+var path = require("path");
+
 var commander = require("commander");
 
 var version = require("../package.json").version;
+
+var Game = require("./game/index.js");
 
 commander
     .version(version);
@@ -8,8 +12,16 @@ commander
 commander
     .command("server")
     .description("Main game server")
+    .option("-c, --config <path>",
+            "Set the loacation of the config file. Defaults to ./config.json")
     .action(function(env) {
-        console.log(env);
+        // Parse config file
+        var configFileLocation =
+            env.config || path.join(process.cwd(), "./config.json");
+        var config = require(configFileLocation);
+
+        // Start a game
+        var game = new Game(config.servers[0].users[0].key, 20);
     });
 
 commander
