a3aef61e696f8b4bb784111bf87c0493fc78e79d
Gio d'Amelio <giodamelio@gmail.com>
2015-01-14 18:06:39 -0800

    Generators are awesome

--- src/commands/server.js
+++ src/commands/server.js
@@ -2,6 +2,7 @@ var path = require("path");
 
 var r = require("rethinkdb");
 var jayschema = require("jayschema");
+var suspend = require("suspend");
 
 var Game = require("../game/game");
 var Runner = require("../game/runner");
@@ -13,61 +14,70 @@ module.exports = function(commander, log) {
         .command("server")
         .description("Main game server")
         .option("-c, --config <path>", "Set the loacation of the config file. Defaults to ./config.json")
-        .action(function(env) {
+        .action(suspend(function*(env) {
             // Parse config file
             var configFileLocation =
                 env.config || path.join(process.cwd(), "./config.json");
             var config = require(configFileLocation);
 
-            //Connect to rethinkdb
-            r.connect({
+            // Connect to rethinkdb
+            var conn = yield r.connect({
                 db: "vindiniumBots"
-            }).then(function(conn) {
-                // Query the db every second and check for new games to start
-                setInterval(() => {
-                    r
-                        .table("Games")
-                        .filter(
-                            r.row("status").eq("pending")
-                        )
-                        .run(conn)
-                        .then(function(cursor) {
-                            cursor.each(function(error, pendingGame) {
-                                if (error) {
-                                    log.error(error);
-                                    return;
-                                }
-
-                                // Validate the game
-                                 var validator = new jayschema();
-                                 var errs = validator.validate(pendingGame, GameSchema);
-                                 if (errs.length > 0) {
-                                     log.error(errs);
-                                     return;
-                                 }
-
-                                 // Change status to playing
-                                 r
-                                     .table("Games")
-                                     .get(pendingGame.id)
-                                     .update({
-                                         status: "playing"
-                                     })
-                                    .run(conn)
-                                    .then(function() {
-                                        // Start a game
-                                        var game = new Game(pendingGame);
-                                        var runner = new Runner({
-                                            game, log
-                                        });
-                                        runner.start();
-                                    });
-                            });
-                        });
-                }, 1000);
-            }).error(function(error) {
-                throw error;
             });
-        });
+
+            // Query the db every second and check for new games to start
+            var id = setInterval(suspend(function*() {
+                // Get all pending games
+                var cursor = yield r
+                    .table("Games")
+                    .filter({
+                        status: "pending"
+                    }).run(conn);
+
+                try {
+                    // Get one game
+                    var gameInfo = yield cursor.next();
+
+                    // Validate the game info
+                    var validator = new jayschema();
+                    var errs = validator.validate(gameInfo, GameSchema);
+                    if (errs.length > 0) {
+                        throw errs;
+                    }
+
+                    // Set the status to playing
+                    yield r
+                        .table("Games")
+                        .get(gameInfo.id)
+                        .update({
+                            status: "playing"
+                        }).run(conn);
+
+                    // Create a new game
+                    var game = new Game(gameInfo);
+                    var runner = new Runner({
+                        log, game
+                    });
+                    runner.start();
+                } catch (err) {
+                    if (err.name === "RqlDriverError" &&
+                            err.message === "No more rows in the cursor.") {
+                        return;
+                    } else {
+                        log.error(err);
+                    }
+                } finally {
+                    cursor.close();
+                }
+            }), 1000);
+
+            // Listen for Control-C and close nicely
+            process.on("SIGINT", function() {
+                log.info("Shutting down");
+                clearInterval(id);
+                conn.close();
+                process.exit();
+            });
+        }));
 };
 
