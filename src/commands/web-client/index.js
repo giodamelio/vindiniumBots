b7f65ce083fda99ad3ed33998137edc07517fdd2
Gio d'Amelio <giodamelio@gmail.com>
2015-01-15 19:55:29 -0800

    Koa hello world!

--- src/commands/web-client/index.js
+++ src/commands/web-client/index.js
@@ -1,10 +1,21 @@
+var koa = require("koa");
+var koaStatic = require("koa-static");
+var koaMount = require("koa-mount");
+
 module.exports = function(commander, log) {
     commander
         .command("web-client")
         .description("Web client")
         .option("-a, --auth <path>", "Set the loacation of the auth file. Defaults to ./auth.json")
         .action(function(env) {
-            log.info("Hello World!");
+            var server = koa();
+
+            server.use(function* () {
+                this.body = "Hello World!";
+            });
+
+            server.listen(3141);
+            log.info("Server listening on http://localhost:3141");
         });
 };
 
