92ce31f0189556fef51acca3b19593df08492e4d
Gio d'Amelio <giodamelio@gmail.com>
2015-01-24 18:00:40 -0800

    Remove rethinkdb and start mongodb with mongoose

--- src/commands/server/index.js
+++ src/commands/server/index.js
@@ -1,7 +1,6 @@
 var path = require("path");
 
-var r = require("rethinkdb");
-var jayschema = require("jayschema");
+var mongoose = require("mongoose");
 var suspend = require("suspend");
 
 var Game = require("../../game/game");
@@ -20,62 +19,23 @@ module.exports = function(commander, log) {
                 env.config || path.join(process.cwd(), "./config.json");
             var config = require(configFileLocation);
 
-            // Connect to rethinkdb
-            var conn = yield r.connect({
-                db: "vindiniumBots"
-            });
-
+            // Connect to mongodb
+            mongoose.connect("mongodb://localhost/vindiniumBots");
+           
             // Query the db every second and check for new games to start
             var id = setInterval(suspend(function*() {
-                // Get all pending games
-                var cursor = yield r
-                    .table("Games")
-                    .filter({
-                        status: "pending"
-                    }).run(conn);
-
-                try {
-                    // Get one game
-                    var gameInfo = yield cursor.next();
-
-                    // Validate the game info
-                    var validator = new jayschema();
-                    var errs = validator.validate(gameInfo, GameSchema);
-                    if (errs.length > 0) {
-                        throw errs;
-                    }
-
-                    // Set the status to playing
-                    yield r
-                        .table("Games")
-                        .get(gameInfo.id)
-                        .update({
-                            status: "playing"
-                        }).run(conn);
-
-                    // Create a new game
-                    var game = new Game(gameInfo);
-                    var runner = new Runner({
-                        log, game
-                    });
-                    runner.start();
-                } catch (err) {
-                    if (err.name === "RqlDriverError" &&
-                            err.message === "No more rows in the cursor.") {
-                        return;
-                    } else {
-                        log.error(err);
-                    }
-                } finally {
-                    cursor.close();
-                }
+                // Create a new game
+                // var game = new Game({});
+                // var runner = new Runner({
+                //     log, game
+                // });
+                // runner.start();
             }), 1000);
 
             // Listen for Control-C and close nicely
             process.on("SIGINT", function() {
                 log.info("Shutting down");
                 clearInterval(id);
-                conn.close();
                 process.exit();
             });
         }));
--- src/game/game.js
+++ src/game/game.js
@@ -1,18 +1,9 @@
-var jayschema = require("jayschema");
-
 var BaseBot = require("./baseBot");
 var GameSchema = require("../schemas/game");
 
 class Game {
     // Create a new game
     constructor(options) {
-        // Validate options
-        var validator = new jayschema();
-        var errs = validator.validate(options, GameSchema);
-        if (errs.length > 0) {
-            throw err;
-        }
-
         // Add options to class
         this.key = options.key;
         this.server_url = options.server_url;
