3b4256d30c0833b8a67ded492576792ff1240ecc
Gio d'Amelio <giodamelio@gmail.com>
2015-01-28 21:27:55 -0800

    Basic react class

--- src/commands/web-client/static/js/main.js
+++ src/commands/web-client/static/js/main.js
@@ -1,5 +1,15 @@
+var SayHello = React.createClass({
+    render: function(){
+        if (this.props.name) {
+            return React.DOM.h1(null, "Hello " + this.props.name + "!");
+        } else {
+            return React.DOM.h1(null, "Hello World!");
+        }
+    }
+});
+
 React.renderComponent(
-    React.DOM.h1(null, "Hello World!"),
+    SayHello({name: "Gio"}),
     document.getElementById("app")
 );
 
