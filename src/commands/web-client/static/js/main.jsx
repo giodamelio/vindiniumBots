15cf09e0dd9988fad5f76b62e48665d3ed594310
Gio d'Amelio <giodamelio@gmail.com>
2015-01-29 12:26:59 -0800

    Setup linter

--- src/commands/web-client/static/js/main.jsx
+++ src/commands/web-client/static/js/main.jsx
@@ -1,9 +1,13 @@
 var SayHello = React.createClass({
     render: function(){
         if (this.props.name) {
-            return <h1>Hello {this.props.name}!</h1>
+            return (
+                <h1>Hello {this.props.name}!</h1>
+            );
         } else {
-            return <h1>Hello World!</h1>
+            return (
+                <h1>Hello World!</h1>
+            );
         }
     }
 });
